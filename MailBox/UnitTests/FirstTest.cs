b465b5fe274e5ed39ff66d1bbd1a1f896d663389
maxization <damian.opoka99@gmail.com>
2020-11-02 13:07:17 +0100

    Add xUnit

---
+++ MailBox/UnitTests/FirstTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+using MailBox;
+using MailBox.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTests
+{
+    public class MailControllerTests
+    {
+        [Fact]
+        public void ReturnedValueOfIndexTest()
+        {
+            var controller = new MailController();
+            var result = controller.Index();
+            Assert.IsType<ViewResult>(result);
+            
+        }
+    }
+}
