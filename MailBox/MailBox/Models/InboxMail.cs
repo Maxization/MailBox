197ea2211c8be703ab44d0947ad8e37ec9bff48e
ozel981 <ozel981@interia.pl>
2020-11-08 17:41:17 +0100

    Mail models

---
+++ MailBox/MailBox/Models/InboxMail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailBox.Models
+{
+    [Serializable]
+    public class InboxMail
+    {
+        public bool Read { get; set; }
+        public User Sender { get; }
+        public List<User> Recipients { get; }
+        public string Topic { get; }
+        public string Text { get; }
+        public DateTime Date { get; }
+        public InboxMail MailReply { get; }
+
+        public InboxMail(bool Read, User Sender, List<User> Recipients, string Topic, string Text, DateTime date, InboxMail MailReply)
+        {
+            this.Read = Read;
+            this.Sender = Sender;
+            this.Recipients = Recipients;
+            this.Topic = Topic;
+            this.Text = Text;
+            this.Date = Date;
+            this.MailReply = MailReply;
+        }
+    }
+}
---
+++ MailBox/MailBox/Models/NewMail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailBox.Models
+{
+    public class NewMail
+    {
+        public User Sender { get; set; }
+        public List<User> CCRecipients { get; set; }
+        public List<User> BCRecipients { get; set; }
+        public string Topic { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public InboxMail MailReply { get; }
+        public NewMail(User Sender, List<User> CCRecipients, List<User> BCRecipients, string Topic, string Text, DateTime Date, InboxMail MailReply)
+        {
+            this.Sender = Sender;
+            this.CCRecipients = CCRecipients;
+            this.BCRecipients = BCRecipients;
+            this.Topic = Topic;
+            this.Text = Text;
+            this.Date = Date;
+            this.MailReply = MailReply;
+        }
+    }
+}
