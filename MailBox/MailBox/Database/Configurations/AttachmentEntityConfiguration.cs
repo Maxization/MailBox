8f5bab4ad93f75fc8c0d186bf9c1cb829542a373
adassimo25 <adasimad99@gmail.com>
2021-01-02 16:38:31 +0100

    Attachment database model and configuration

---
+++ MailBox/MailBox/Database/Configurations/AttachmentEntityConfiguration.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MailBox.Database.Configurations
+{
+    public class AttachemtEntityConfiguration : IEntityTypeConfiguration<Attachment>
+    {
+        public void Configure(EntityTypeBuilder<Attachment> builder)
+        {
+            builder.HasKey(c => new { c.ID });
+
+            builder.Property(c => c.MailID)
+                .IsRequired();
+
+            builder.Property(c => c.Filename)
+                .IsRequired();
+
+            builder.HasOne(c => c.Mail)
+                .WithMany(c => c.Attachments)
+                .HasForeignKey(c => c.MailID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
---
+++ MailBox/MailBox/Database/DatabaseModels/Attachment.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MailBox.Database
+{
+    public class Attachment
+    {
+        public int ID { get; set; }
+        public int MailID { get; set; }
+        public Mail Mail { get; set; }
+        public string Filename { get; set; }
+
+    }
+}
